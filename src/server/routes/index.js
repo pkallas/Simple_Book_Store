7b4adcaa63a1618b7ba6ba0b13922b8d2b143045
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-18 16:38:40 -0700

    Added first get route to routes, will update index.ejs next

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const books = require('../models/db/books');
 
 router.get('/', (request, response) => {
-  response.render('books/index');
+  books.getAllBooks()
+  .then(books => response.render('books/index', books))
+  .catch(error => console.error(error));
 });
 
 module.exports = router;
