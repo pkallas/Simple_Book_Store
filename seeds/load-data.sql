e5f0d598db172b7c9d22e83e7c2aa3edca5237d3
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-18 12:56:20 -0700

    Added a command and files to load data into the database to start

---
+++ seeds/load-data.sql
@@ -0,0 +1,7 @@
+\c simple_book_store
+
+COPY books (title, img_url, price, in_stock, isbn, publisher) FROM '/Users/pkallas/Desktop/Simple_Book_Store/Simple_Book_Store/seeds/csv/books.csv' DELIMITERS ',' CSV;
+COPY authors (first_name, last_name) FROM '/Users/pkallas/Desktop/Simple_Book_Store/Simple_Book_Store/seeds/csv/authors.csv' DELIMITERS ',' CSV;
+COPY genres (name) FROM '/Users/pkallas/Desktop/Simple_Book_Store/Simple_Book_Store/seeds/csv/genres.csv' DELIMITERS ',' CSV;
+COPY authors_books (author_id, book_id) FROM '/Users/pkallas/Desktop/Simple_Book_Store/Simple_Book_Store/seeds/csv/authors_books.csv' DELIMITERS ',' CSV;
+COPY genres_books (genre_id, book_id) FROM '/Users/pkallas/Desktop/Simple_Book_Store/Simple_Book_Store/seeds/csv/genres_books.csv' DELIMITERS ',' CSV;
