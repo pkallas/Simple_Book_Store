82b91687fd8a210ad6971a250133c767904a06ca
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-27 18:01:08 -0700

    Added session handling to routes

--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -23,81 +23,97 @@ router.get('/books/search', (request, response) => {
 });
 
 router.get('/books/create', (request, response) => {
-  response.render('books/create');
+  if (request.session.role === 'admin') {
+    response.render('books/create');
+  } else {
+    response.redirect('/');
+  }
 });
 
 router.post('/books/create', (request, response) => {
-  const compiledBook = {
-    title: request.body.title,
-    price: request.body.price,
-    image: request.body.image,
-    inStock: request.body.inStock,
-    isbn: request.body.isbn,
-    publisher: request.body.publisher,
-  };
-  const authors = [];
-  const genres = [];
-  let i = 0;
-  while (request.body['firstName' + i]) {
-    let author = {};
-    author.firstName = request.body['firstName' + i];
-    author.lastName = request.body['lastName' + i];
-    authors.push(author);
-    i++;
-  };
-  let j = 0;
-  while (request.body['genre' + j]) {
-    genres.push(request.body['genre' + j]);
-    j++;
-  };
-  books.createBook(compiledBook)
-    .then(book => {
-      books.addOrEditAuthors(book[0].id, authors)
-      .then(() => books.addOrEditGenres(book[0].id, genres))
-      .then(() => response.redirect(`/books/${book[0].id}`));
-    })
-    .catch(error => console.error(error));
+  if (request.session.role === 'admin') {
+    const compiledBook = {
+      title: request.body.title,
+      price: request.body.price,
+      image: request.body.image,
+      inStock: request.body.inStock,
+      isbn: request.body.isbn,
+      publisher: request.body.publisher,
+    };
+    const authors = [];
+    const genres = [];
+    let i = 0;
+    while (request.body['firstName' + i]) {
+      let author = {};
+      author.firstName = request.body['firstName' + i];
+      author.lastName = request.body['lastName' + i];
+      authors.push(author);
+      i++;
+    };
+    let j = 0;
+    while (request.body['genre' + j]) {
+      genres.push(request.body['genre' + j]);
+      j++;
+    };
+    books.createBook(compiledBook)
+      .then(book => {
+        books.addOrEditAuthors(book[0].id, authors)
+        .then(() => books.addOrEditGenres(book[0].id, genres))
+        .then(() => response.redirect(`/books/${book[0].id}`));
+      })
+      .catch(error => console.error(error));
+  } else {
+    response.status(401).send('Unauthorized user');
+  }
 });
 
 router.get('/books/:id/edit', (request, response) => {
-  const id = request.params.id;
-  books.getOneBook(id)
-  .then(book => {
-    books.getAllGenres().then(allGenres => response.render('books/edit', { book, allGenres }));
-  })
-  .catch(error => console.error(error));
+  if (request.session.role === 'clerk' || request.session.role === 'admin') {
+    const id = request.params.id;
+    books.getOneBook(id)
+    .then(book => {
+      books.getAllGenres().then(allGenres => response.render('books/edit', { book, allGenres }));
+    })
+    .catch(error => console.error(error));
+  } else {
+    response.redirect('/');
+  }
 });
 
 router.put('/books/:id/edit', (request, response) => {
-  const compiledBook = {
-    title: request.body.title,
-    image: request.body.image,
-    price: request.body.price,
-    inStock: request.body.inStock,
-    isbn: request.body.isbn,
-    publisher: request.body.publisher,
-    id: request.params.id,
-  };
-  const authors = [];
-  const genres = [];
-  let i = 0;
-  while (request.body['firstName' + i]) {
-    let author = {};
-    author.firstName = request.body['firstName' + i];
-    author.lastName = request.body['lastName' + i];
-    authors.push(author);
-    i++;
-  };
-  let j = 0;
-  while (request.body['genre' + j]) {
-    genres.push(request.body['genre' + j]);
-    j++;
-  };
-  books.updateBook(compiledBook)
-  .then(() => books.addOrEditAuthors(compiledBook.id, authors))
-  .then(() => books.addOrEditGenres(compiledBook.id, genres))
-  .then(() => response.redirect(`/books/${compiledBook.id}`))
-  .catch(error => console.error(error));
+  if (request.session.role === 'clerk' || request.session.role === 'admin') {
+    const compiledBook = {
+      title: request.body.title,
+      image: request.body.image,
+      price: request.body.price,
+      inStock: request.body.inStock,
+      isbn: request.body.isbn,
+      publisher: request.body.publisher,
+      id: request.params.id,
+    };
+    const authors = [];
+    const genres = [];
+    let i = 0;
+    while (request.body['firstName' + i]) {
+      let author = {};
+      author.firstName = request.body['firstName' + i];
+      author.lastName = request.body['lastName' + i];
+      authors.push(author);
+      i++;
+    };
+    let j = 0;
+    while (request.body['genre' + j]) {
+      genres.push(request.body['genre' + j]);
+      j++;
+    };
+    books.updateBook(compiledBook)
+    .then(() => books.addOrEditAuthors(compiledBook.id, authors))
+    .then(() => books.addOrEditGenres(compiledBook.id, genres))
+    .then(() => response.redirect(`/books/${compiledBook.id}`))
+    .catch(error => console.error(error));
+  } else {
+    response.status(401).send('Unauthorized user');
+  }
 });
 
 router.get('/books/:id', (request, response) => {
