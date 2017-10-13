7dc54c8dd4d36056a3d9ee1c4ca34ecc60359a22
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-13 14:00:22 -0700

    Added tests for books/create route

--- test/end-to-end/books.test.js
+++ test/end-to-end/books.test.js
@@ -44,16 +44,92 @@ context('Book routes', function () {
 
     it('Should render the page when user is an admin', function () {
       let agent = chai.request.agent(app);
-      agent.post('/login')
+      return agent.post('/login')
       .set('content-type', 'application/x-www-form-urlencoded')
       .send({ login: 'bob',
               password: 'badpassword', })
       .then(response => {
-        expect(response).to.have.cookie('cookie.sid');
-
         return agent.get('/books/create')
         .then(response => {
           expect(response).to.have.status(200);
+        });
+      });
+    });
+
+    it('Should respond with a 401 when a user that is not an admin tries to create a book', function () {
+      return request(app)
+      .post('/books/create')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({ title: 'New Book',
+              price: 3.43,
+              image: 'http://NewImage.jpg',
+              inStock: 30,
+              isbn: 'New ISBN',
+              publisher: 'New Publisher',
+              firstName0: 'First Author',
+              lastName0: 'First Author',
+              firstName1: 'Second Author',
+              lastName1: 'Second Author',
+              genre0: 'First genre',
+              genre1: 'Second genre', })
+      .then(response => {
+        expect(response).to.have.status(401);
+      })
+      .catch(error => {
+        expect(error.response).to.have.status(401);
+      });
+    });
+
+    it('Should redirect to the newly created book page if user is an admin', function () {
+      let agent = chai.request.agent(app);
+      return agent.post('/login')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({ login: 'bob',
+              password: 'badpassword', })
+      .then(response => {
+        return agent.post('/books/create')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ title: 'New Book',
+                price: 3.43,
+                image: 'http://NewImage.jpg',
+                inStock: 30,
+                isbn: 'New ISBN',
+                publisher: 'New Publisher',
+                firstName0: 'First Author',
+                lastName0: 'First Author',
+                firstName1: 'Second Author',
+                lastName1: 'Second Author',
+                genre0: 'First genre',
+                genre1: 'Second genre', })
+        .then(response => {
+          expect(response.redirects[0]).to.match(/\/books\/4/);
+        });
+      });
+    });
+
+    it('Should render the create page with a message if the image url does not start with http or end with .jpg or .png', function () {
+      let agent = chai.request.agent(app);
+      return agent.post('/login')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({ login: 'bob',
+              password: 'badpassword', })
+      .then(response => {
+        return agent.post('/books/create')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ title: 'New Book',
+                price: 3.43,
+                image: 'NewImage',
+                inStock: 30,
+                isbn: 'New ISBN',
+                publisher: 'New Publisher',
+                firstName0: 'First Author',
+                lastName0: 'First Author',
+                firstName1: 'Second Author',
+                lastName1: 'Second Author',
+                genre0: 'First genre',
+                genre1: 'Second genre', })
+        .then(response => {
+          expect(response.res.text).to.include('Please input a valid image');
         });
       });
     });
